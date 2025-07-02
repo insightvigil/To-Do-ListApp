cf8bcdf16d1d32e69f335b4da6f1649d29bb9a54
Adrian Flores <adrianfloresvigil@gmail.com>
2025-07-01 22:35:26 -0600

    Función Add Task Creada

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,27 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container")
+
+//Función AddTask
+
+function addTask() {
+  const task = inputBox.value.trim();
+
+  if (!task) {
+    alert("Please write down a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  //innerHTML nos permite inserta contenido HTML de forma dínamica en algun elemento del 
+  //DOM en este caso el li
+  li.innerHTML = `
+  <label>
+    <input type="checkbox">
+    <span>${task}</span>
+  </label>
+  <span class="edit-btn">Edit</span>
+  <span class="delete-btn">Delete</span>
+  `;
+
+}
