fde0403bf8053f9e4ea2443f3dc0a1366facdb75
Adrián Vigil <insightvigil@gmail.com>
2025-07-02 19:56:31 -0600

    Función btneditar agregada

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-//Variables Selectoras, se usa el querySelector por que selecciona el elemento html con esa clase o selector css.
-const checkbox = li.querySelector("input");
-const editBtn = li.querySelector(".edit-btn");
-const taskSpan = li.querySelector("span");
-const deleteBtn = li.querySelector(".delete-btn");
+
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
+
+
 
 //Función AddTask
 
@@ -29,6 +29,28 @@ function addTask() {
   <span class="delete-btn">Delete</span>
   `;
 
+  //Variables Selectoras, se usa el querySelector por que selecciona el elemento html con esa clase o selector css.
+  const checkbox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const taskSpan = li.querySelector("span");
+  const deleteBtn = li.querySelector(".delete-btn");
+
+
+  checkbox.addEventListener("click", ()=> {
+    li.classList.toggle("completed", checkbox.checked);
+  })
+
+//Función Edit Button
+editBtn.addEventListener("click", function () {
+  const update = prompt("Edit task:", taskSpan.textContent);
+  if (update !== null) {
+    taskSpan.textContent = update;
+    li.classList.remove("completed");
+  }
+});
+
+
+
   //appendChild sirve para agregar un nuevo nodo(elemento) como hijo de otro nodo(elemento) del Dom
   //NOTA: En javaScript no es necesario poner el ; al fnalizar una linea de codigo, pero aveces puede causar problemas al utilizar herramientas como ESLint y puedo ocacionar bugs dificiles de encontrar
   //SINTAXIS: padre.appendChild(hijo);
@@ -36,9 +58,6 @@ function addTask() {
   inputBox.value = "";
 }
 
-checkbox.addEventListener("click", ()=> {
-    li.classList.toggle("completed", checkbox.checked);
-})
 
 
 
